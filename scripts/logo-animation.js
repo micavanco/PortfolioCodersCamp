c6739a4078736fd85e667a14b18b486a07e2a10e
Micavanco <micavanco@gmail.com>
2019-08-06 09:10:46 +0200

    Added the rest of logo parts, fixed the whole logo animation and fixed responsivity

--- scripts/logo-animation.js
+++ scripts/logo-animation.js
@@ -10,6 +10,14 @@
         frames[2].style.right = '-200px';
         frames[3].style.top = '200px';
         frames[3].style.right = '-200px';
+        frames[4].style.top = '-320px';
+        frames[4].style.left = '-320px';
+        frames[5].style.top = '-320px';
+        frames[5].style.right = '-320px';
+        frames[6].style.top = '320px';
+        frames[6].style.right = '-320px';
+        frames[7].style.top = '320px';
+        frames[7].style.right = '-320px';
 
         setTimeout(()=>{
             frames.forEach(e=>e.style.opacity = '1');
@@ -21,9 +29,17 @@
             frames[2].style.right = '0px';
             frames[3].style.top = '170px';
             frames[3].style.left = '0px';
+            frames[4].style.top = '120px';
+            frames[4].style.left = '0px';
+            frames[5].style.top = '120px';
+            frames[5].style.right = '0px';
+            frames[6].style.top = '255px';
+            frames[6].style.right = '0px';
+            frames[7].style.top = '290px';
+            frames[7].style.left = '0px';
         },500);
 
-        setTimeout(()=>frames.forEach(e=>e.style.opacity = '0'),700);
+        setTimeout(()=>frames.forEach(e=>e.style.opacity = '0'),1500);
 
         let i = 0;
         let j = 0;
@@ -36,10 +52,8 @@
                 frames[i++].style.opacity = '1';
 
 
-                if(i === 4)
+                if(i === 8)
                     i = 0;
-
-
 
             },500);
         },800);
